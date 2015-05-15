72ce50994bf0f82f30e683aec0c344d20111a7ca
fantastickford1 <carlitos_zenteno@hotmail.com>
2015-05-14 20:05:32 -0500

    JAVA1Program
    Multiplataforma con operaciones simples

---
+++ FirstHomework.java
@@ -0,0 +1,15 @@
+public class FirstHomework{
+	public static void main(String argv[]){
+		System.out.println("Primer programa multi plataforma");
+		int numero=2;
+		int numero2=5;
+		int suma;
+
+		suma = numero + numero2;
+		System.out.println("La suma es:" + suma);
+		int multiplicacion = numero * numero2;
+		System.out.println("La multiplicacion es:" + multiplicacion);
+		float divicion = numero2 / numero;
+		System.out.println("La divicion es:" + divicion);
+	}
+}
